b6132f7626ebbdb0393d188e060519084c885aab
Ericode99 <eric.rudischhauser@gmail.com>
2022-03-06 20:37:56 +0100

    Added recommendation feature

--- js/script.js
+++ js/script.js
@@ -109,43 +109,82 @@ document.addEventListener("keydown", function (event) {
 // SELECTORS
 const countrySelector = document.querySelector(".country-selector");
 const currencyDisplay = document.querySelectorAll(".currency");
+const recommendationText = document.querySelector(".recommendation-text");
+const recommendation = document.querySelector(".recommendation");
 
 countrySelector.addEventListener("change", function () {
   let currency = "$";
   console.log(countrySelector.value);
+  const showRecommendation = function (text) {
+    recommendation.classList.remove("hidden");
+    recommendationText.textContent = text;
+  };
 
   if (countrySelector.value === "australia") {
     currency = "A$";
+    showRecommendation(
+      "In general, Australians don't tip. Tipping in tourist areas is getting more common, here you can leave a tip of 5-10%. But if you don't feel like tipping, you don't have to, even in tourist areas."
+    );
   }
   if (countrySelector.value === "canada") {
     currency = "C$";
+    showRecommendation(
+      "Tipping in Canada is expected. Tip 15% for normal service and 20% for exceptional service."
+    );
   }
   if (countrySelector.value === "china") {
     currency = "¥";
+    showRecommendation(
+      "Tipping in China is generally uncommon and can even be considered rude or embarrassing in some circumstances."
+    );
   }
   if (countrySelector.value === "england") {
     currency = "£";
+    showRecommendation(
+      "It is customary to leave 10 to 15% of the bill as tip when eating out, though some restaurants add on a service charge instead. At pubs, don't tip unless you get table service."
+    );
   }
   if (countrySelector.value === "eu") {
     currency = "€";
+    showRecommendation(
+      "Tipping in Europe is optional. Ofthe the service fee is included in the price of the food. If you want you can tip 5-10% for good service. 10% is already considered a big tip."
+    );
   }
   if (countrySelector.value === "mexico") {
     currency = "Mex$";
+    showRecommendation(
+      "In Mexico, 15% of the bill is a good rule of thumb to follow; if you're in a big group you may find that a service fee is automatically added. If you feel you have been given particularly great service, 20% will be greatly appreciated."
+    );
   }
   if (countrySelector.value === "usa") {
     currency = "$";
+    showRecommendation(
+      "In the United States tipping is expected. The service personel earn most of their living off of tips. 15% is standart and 20-25% is for good service. At nice restaurants waiters might even expect 20-30%."
+    );
   }
   if (countrySelector.value === "southamerica") {
     currency = "$";
+    showRecommendation(
+      "Many locals don't tip in small familyrun restaurants. However, a small tip is always appreciated. In upscale establishments and in tourist areas tipping is more common. As a general rule of thumb 10% works in most places. You can tip more or less depending on how the quality of the service was."
+    );
   }
   if (countrySelector.value === "southafrica") {
     currency = "R";
+    showRecommendation(
+      "It is customary to tip good service in South Africa. An acceptable amount is 10-15%. If you have six or more in your party, restaurants will generally add a gratuity to your bill."
+    );
   }
   if (countrySelector.value === "switzerland") {
     currency = "CHF";
+    showRecommendation(
+      "You never have to worry about tipping in Switzerland, as tips are included in the price. However, a tip of 5-10% for good service is appreciated."
+    );
   }
   if (countrySelector.value === "rest") {
     currency = "$";
+    if (!recommendation.classList.contains("hidden")) {
+      recommendation.classList.add("hidden");
+    }
   }
 
   currencyDisplay.forEach((el) => (el.textContent = currency));
